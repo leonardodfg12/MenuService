d7be847b6ad75c1d57b11bbecce0364e244c137e
leonardo.rodrigues <leonardo.rodrigues@softplan.com.br>
2025-07-21 02:39:11 -0300

    autenticação nos endpoints

--- src/MenuService.API/Controllers/MenuServiceController.cs
+++ src/MenuService.API/Controllers/MenuServiceController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using MenuService.Application.Commands;
 using MenuService.Application.Handlers;
 using MenuService.Domain.Entities;
+using Microsoft.AspNetCore.Authorization;
 
 namespace MenuService.API.Controllers;
 
@@ -9,6 +10,7 @@ namespace MenuService.API.Controllers;
 [Route("/[controller]/[action]")]
 public class MenuServiceController(MenuServiceHandler handler) : ControllerBase
 {
+    [Authorize(Roles = "Admin, Cozinha")]
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateMenuItemCommand command)
     {
@@ -16,9 +18,11 @@ public class MenuServiceController(MenuServiceHandler handler) : ControllerBase
         return Ok(new { message = "Item criado com sucesso", item = command });
     }
 
+    [Authorize(Roles = "Admin, Cozinha")]
     [HttpGet]
     public async Task<IEnumerable<MenuItem>> GetAll() => await handler.GetAllAsync();
 
+    [Authorize(Roles = "Admin, Cozinha")]
     [HttpGet("{id}")]
     public async Task<ActionResult<MenuItem>> GetById(string id)
     {
@@ -27,6 +31,7 @@ public class MenuServiceController(MenuServiceHandler handler) : ControllerBase
         return item;
     }
 
+    [Authorize(Roles = "Admin, Cozinha")]
     [HttpPut("{id}")]
     public async Task<IActionResult> Update(string id, [FromBody] CreateMenuItemCommand command)
     {
@@ -34,6 +39,7 @@ public class MenuServiceController(MenuServiceHandler handler) : ControllerBase
         return NoContent();
     }
 
+    [Authorize(Roles = "Admin, Cozinha")]
     [HttpDelete("{id}")]
     public async Task<IActionResult> Delete(string id)
     {
